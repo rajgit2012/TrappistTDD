d2cc6e2f1c4c841530adae51896aefc8298fa22b
Raj Forhad <info@elpicsoft.com>
2018-01-06 00:23:11 +0000

    Pushing newly created app with presenter and test presenter

--- app/src/test/java/com/trappist/logintdd/presenter/LoginPresenterTest.java
+++ app/src/test/java/com/trappist/logintdd/presenter/LoginPresenterTest.java
@@ -3,14 +3,17 @@ package com.trappist.logintdd.presenter;
 import org.junit.After;
 import org.junit.Before;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by Raj Forhad on 06/01/2018.
  */
 public class LoginPresenterTest {
 
     private LoginPresenter loginPresenter;
+
+    public LoginPresenterTest(LoginPresenter loginPresenter) {
+        this.loginPresenter = loginPresenter;
+    }
+
     @Before
     public void setUp() throws Exception {
     }
