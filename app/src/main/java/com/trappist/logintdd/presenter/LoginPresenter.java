81d9df6448a8e422c1037f7a01caa301400a2346
Raj Forhad <info@elpicsoft.com>
2018-01-06 00:06:26 +0000

    First commit

---
+++ app/src/main/java/com/trappist/logintdd/presenter/LoginPresenter.java
@@ -0,0 +1,8 @@
+package com.trappist.logintdd.presenter;
+
+/**
+ * Created by Raj Forhad on 06/01/2018.
+ */
+
+public class LoginPresenter {
+}
---
+++ app/src/test/java/com/trappist/logintdd/presenter/LoginPresenterTest.java
@@ -0,0 +1,21 @@
+package com.trappist.logintdd.presenter;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Raj Forhad on 06/01/2018.
+ */
+public class LoginPresenterTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+}
